c2681f6bc8232e72bf955ce0a8c7939a604f516e
wuhy <sparklewhy@gmail.com>
2015-12-01 20:21:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+fisx-command-uninstall
+========
---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * @file 入口模块
+ * @author sparklewhy@gmail.com
+ */
+
+var pkgManage = require('fisx-package');
+
+exports.name = 'install <components>';
+exports.desc = 'install components package';
+exports.options = {
+    '-h, --help': 'print this help message',
+    '-r, --root <path>': 'set project root',
+    '-s, --save': 'save component(s) dependencies into `package.json` file',
+    '-d, --save-dev': 'save component(s) dependencies into `package.json` devDependencies'
+};
+
+exports.run = function (argv, cli, env) {
+    if (argv.h || argv.help) {
+        return cli.help(exports.name, exports.options);
+    }
+
+    argv._.shift();
+    var removeComponents = argv._;
+    var options = {
+        root: env.cwd,
+        saveToDevDep: argv['save-dev'] || argv.d,
+        saveToDep: argv.save || argv.s
+    };
+    return pkgManage.initProjectRoot(env.configNameSearch[0], options, fis)
+        .then(pkgManage.loadUserConfig.bind(this, env.configNameSearch[0], options, fis))
+        .then(function () {
+            return pkgManage.uninstall(removeComponents, options);
+        });
+};
